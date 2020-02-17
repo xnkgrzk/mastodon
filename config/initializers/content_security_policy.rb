5e66023946a3d5dbebe58683d38e46645f78c72b
xionkgrzk <xionkgrzk@mk39.xyz>
2020-02-17 22:42:27 +0900

    Replace domain of InstanceTicker theme provider

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -16,7 +16,7 @@ media_host ||= host_to_url(ENV['S3_CLOUDFRONT_HOST'])
 media_host ||= host_to_url(ENV['S3_HOSTNAME']) if ENV['S3_ENABLED'] == 'true'
 media_host ||= assets_host
 
-instance_ticker_host = "https://pla.jp"
+instance_ticker_host = "https://wee.jp"
 
 Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
